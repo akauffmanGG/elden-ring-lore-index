10df3f787960ee3c9f24691bffe67c2916993ab9
Andy Kauffman <schlefty@gmail.com>
2022-05-18 16:13:38 -0400

    create table of contents

---
+++ index.js
@@ -0,0 +1,78 @@
+function initialize() {
+    buildTableOfContents();
+}
+
+function buildTableOfContents() {
+    const sidebar = document.querySelector('.sidebar');
+    const toc = document.createElement('div');
+    toc.className="table-of-contents";
+
+    const categories = document.querySelectorAll('.main .category');
+    categories.forEach(category => {
+        const titleEl = category.querySelector('h2');
+        const menu = document.createElement('div');
+        menu.className = "menu";
+        
+        const items = category.querySelectorAll('.items h3');
+        if(items.length) {
+            const title = createCollapsibleTitle(titleEl.innerHTML)
+            menu.appendChild(title);
+            
+            menu.appendChild( createContent(items) );
+        } else {
+            const title = createLinkTitle(titleEl.innerHTML, titleEl.id);
+
+            menu.appendChild(title);
+        }
+        
+        toc.appendChild(menu);
+    });
+
+    sidebar.appendChild(toc);
+}
+
+function createCollapsibleTitle(text) {
+    const title = document.createElement('div');
+    title.innerHTML = text;
+    title.className = "menu-title";
+    title.classList.add('collapsible'); 
+    title.addEventListener("click", function() {
+        this.classList.toggle("active");
+
+        let content = this.nextElementSibling;
+        if(content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+
+    return title;
+}
+
+function createLinkTitle(text, id) {
+    const title = document.createElement('a');
+    title.innerHTML = text;
+    title.className = "menu-title";
+    title.href = `#${id}`;
+
+    return title;
+}
+
+function createContent(items) {
+    const content = document.createElement('div');
+    content.className = "menu-content";
+
+    const itemsEl = document.createElement('ul');
+
+    items.forEach(item => {
+        const itemEl = document.createElement('li');
+        itemEl.innerHTML = item.innerHTML;
+
+        itemsEl.appendChild(itemEl);
+    });
+
+    content.appendChild(itemsEl);
+
+    return content;
+}
