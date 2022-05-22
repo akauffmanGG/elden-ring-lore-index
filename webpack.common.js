3b7ea07c4c9b43781754a1c4aa7a5c533aa7abcc
Andy Kauffman <schlefty@gmail.com>
2022-05-22 00:24:39 -0400

    configure dev and prod

--- webpack.common.js
+++ webpack.common.js
@@ -2,17 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true
   },
-  devServer: {
-    static: './dist',
-  },
-  devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.html',
