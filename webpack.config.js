0ed943c17513e27019829de9e1b86dc24452391f
Andy Kauffman <schlefty@gmail.com>
2022-05-22 00:01:37 -0400

    bundle html

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
- module: {
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+  module: {
     rules: [
-        {
-          test: /\.less$/i,
-          use: [
-            // compiles Less to CSS
-            "style-loader",
-            "css-loader",
-            "less-loader",
-          ],
-        },
-        {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-        },
-      ]
- },
-};
+      {
+        test: /\.less$/i,
+        use: [
+          // compiles Less to CSS
+          'style-loader',
+          'css-loader',
+          'less-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  }
+}
